f6ab9dec874eb1b6db441e6ca13ea0bf5733eaf1
Prashanth <prash2912@gmail.com>
2017-06-19 22:14:51 +0200

    Added multi process funtion

--- FaceDetect.py
+++ FaceDetect.py
@@ -1,15 +1,14 @@
-#from imutils.video import VideoStream
 from imutils import face_utils
 import imutils
 import dlib
 import cv2
 
 
-def StartDetection():
+def StartDetection(queue):
 	print("[INFO] loading facial landmark predictor...")
 	detector = dlib.get_frontal_face_detector()
 	predictor = dlib.shape_predictor("models/dlib/shape_predictor_68_face_landmarks.dat")
-	fourcc = cv2.VideoWriter_fourcc(*'XVID')
+	#fourcc = cv2.VideoWriter_fourcc(*'XVID')
 	#outVideo = cv2.VideoWriter('outputMouth.mp4',fourcc, 20.0, (400,225))
 	vs = cv2.VideoCapture(0)
 
@@ -18,16 +17,18 @@ def StartDetection():
 		# have a maximum width of 400 pixels, and convert it to
 		# grayscale
 		ok,frame = vs.read()
+		print("Crash")
 		if not ok:
 			break;
-		frame = imutils.resize(frame, width=400)
+		print("Crash")
+		frame = imutils.resize(frame, width=600)
 	#	height,width,channel = frame.shape
 		#print (height)
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	 
+		print("Crash")
 		# detect faces in the grayscale frame
 		rects = detector(gray, 0)
-			# loop over the face detections
+		print("Crash")	# loop over the face detections
 		for rect in rects:
 			# determine the facial landmarks for the face region, then
 			# convert the facial landmark (x, y)-coordinates to a NumPy
@@ -46,10 +47,11 @@ def StartDetection():
 					cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
 		  
 		# show the frame
-		cv2.imshow("Frame", frame)
-	#	outVideo.write(frame)
-		key = cv2.waitKey(1) & 0xFF
+		queue.put(frame)
+	# 	cv2.imshow("Frame", frame)
+	# #	outVideo.write(frame)
+	# 	key = cv2.waitKey(0)# & 0xFF
 	 
 		# if the `q` key was pressed, break from the loop
-		if key == ord("q"):
-			break
+		# if key == ord("q"):
+		# 	break
--- multiprocess.py
+++ multiprocess.py
@@ -1,7 +1,8 @@
 import os
  
-from multiprocessing import Process
+from multiprocessing import Process,Queue
 import FaceDetect
+import cv2
  
 def doubler(number):
     """
@@ -9,24 +10,38 @@ def doubler(number):
     """
     result = number * 2
     proc = os.getpid()
-    print('{0} doubled to {1} by process id: {2}'.format(
-        number, result, proc))
+    while True:
+        print('{0} doubled to {1} by process id: {2}'.format(number, result, proc))
  
-def detectFaces():
+def detectFaces(queue):
+    print('module name:', __name__)
+    print('parent process:', os.getppid())
+    print('process id:', os.getpid())
 	#Start the face detection
+    FaceDetect.StartDetection(queue)
 
 
+#def tracking():
+    #Start tracking code here
 
 if __name__ == '__main__':
-    numbers = [5, 10, 15, 20, 25]
     procs = []
- 
- 	#Start the face detection:
+    q = Queue()
 
-    for index, number in enumerate(numbers):
-        proc = Process(target=doubler, args=(number,))
-        procs.append(proc)
-        proc.start()
- 
-    for proc in procs:
-        proc.join()
+    detectFaceProc = Process(target=detectFaces,args=(q,))
+    procs.append(detectFaceProc)
+    detectFaceProc.start()
+    while True:
+        cv2.imshow("frame",q.get())
+        cv2.waitKey(1)
+    detectFaceProc.join()
+    # proc = Process(target=doubler,args=(1,))
+    # proc.start()
+    # proc.join()
+ #	trackingProc = Process(target=tracking)
+    #Start the face detection:
+
+    # #for index, number in enumerate(numbers):
+    #     proc = Process(target=doubler, args=(number,))
+    #     procs.append(proc)
+    #     proc.start()
