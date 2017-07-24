181b41a9045d200d50cb6b1f8f3baa25de046188
letrend <simon.trendel@tum.de>
2017-07-24 11:41:56 +0200

    zed camera integration started

---
+++ src/pyyolo/setup_gpu.py
@@ -0,0 +1,14 @@
+from distutils.core import setup, Extension
+import numpy
+
+module = Extension('pyyolo',
+	library_dirs=['.', '/usr/local/cuda/lib64', '/usr/local/'],
+	#libraries=['yolo', 'cuda', 'cudart', 'cublas', 'curand', 'cudnn'],
+	libraries=['yolo', 'cuda', 'cudart', 'cublas', 'curand'],
+	include_dirs=[numpy.get_include(), './darknet/include'],
+	sources = ['module.c'])
+
+setup (name = 'pyyolo',
+	version = '0.1',
+	description = 'YOLO wrapper',
+	ext_modules = [module])
